1ceb54e42c8bdcb38dd94730be4c33123b70f2b2
Robin Ricard <ricard.robin@gmail.com>
2016-09-19 19:06:16 +0200

    Fix lodash typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="typed-graphql" />
 
 declare module 'lodash.isobject' {
-  import main = require('lodash');
-  export = main.isObject;
+  import {isObject} from 'lodash';
+  export = isObject;
 }
 
 declare module 'lodash.isstring' {
-  import main = require('lodash');
-  export = main.isString;
+  import {isString} from 'lodash';
+  export = isString;
 }
