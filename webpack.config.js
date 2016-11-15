815d97e975b92cb80630775fe70aabb2a0ef2523
Srdan Srepfler <schrepfler@gmail.com>
2016-11-15 18:41:43 +0100

    Adding webpack config to create bundle

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  context: __dirname + "/src",
+  entry: './index.ts',
+  output: {
+    path: __dirname + '/browser',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  }
+}
