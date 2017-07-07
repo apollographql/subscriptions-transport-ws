805646c52a49f4b4f3bbc6f736ad4e4afb2959aa
urigo <uri.goldshtein@gmail.com>
2017-07-07 21:31:17 +0200

    chore(deprecation-warning): Improve addGraphQLSubscriptions deprecation warning
    #169

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,8 +10,9 @@ import assign = require('lodash.assign');
 // We will move this into a new package in the future
 export function addGraphQLSubscriptions(networkInterface: any, wsClient: SubscriptionClient): any {
   if (process && process.env && process.env.NODE_ENV !== 'production') {
-    console.warn('This method becomes deprecated in the new package graphql-transport-ws. Start using the ' +
-      'GraphQLTransportWSClient to make queries, mutations and subscriptions over websockets.');
+    console.warn('Notice that addGraphQLSubscriptions method will become deprecated in the new package ' +
+      'graphql-transport-ws that will be released soon. Keep track for the new hybrid network release here: ' +
+      'https://github.com/apollographql/subscriptions-transport-ws/issues/169');
   }
 
   return assign(networkInterface, {
